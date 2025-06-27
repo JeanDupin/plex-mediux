fbf6fe49cc93084719ca8623f67a6f9282e2ca92
Jean DUPIN <jean.dupin69@gmail.com>
2025-06-27 22:23:57 +0200

    Init commit

---
+++ R/upload_poster.R
@@ -0,0 +1,14 @@
+#' Upload posters from the mediux files
+#'
+#' @param file The .yaml file with the MediUX posters.
+#'
+#' @return Verbatim for each poster if it uploaded or not
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' upload_poster("test.yaml")
+#' }
+upload_poster <- function(file){
+  print("INIT")
+}
